c87192b5325e5555c783119f33edfb04b8f504dc
FlorianWeise <florianweise@gmx.de>
2023-03-21 12:07:31 +0100

    Update ConfirmedPage.tsx

--- examples/point-of-sale/src/client/components/pages/ConfirmedPage.tsx
+++ examples/point-of-sale/src/client/components/pages/ConfirmedPage.tsx
@@ -1,26 +1,16 @@
 import { NextPage } from 'next';
 import React from 'react';
 import useEffect from 'react;
-import { BackButton } from '../buttons/BackButton';
 import { TransactionsLink } from '../buttons/TransactionsLink';
 import { PoweredBy } from '../sections/PoweredBy';
 import { Progress } from '../sections/Progress';
 import css from './ConfirmedPage.module.css';
-import { useRouter } from 'next/router';
-
 
 const ConfirmedPage: NextPage = () => {
-    const router = useRouter();
-    
-    async function goback (e) {
-        e.preventDefault();
-        console.log("Clicked");
-    }
     
     return (
         <div className={css.root}>
             <div className={css.header}>
-                <BackButton onClick={goback}>Start Over</BackButton>
                 <TransactionsLink />
             </div>
             <div className={css.main}>
